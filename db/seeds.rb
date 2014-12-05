703af721336b2d26fd8ba9b0652b6a59d7d08479
Valentine <valentin-belonogov@yandex.ru>
2014-12-05 17:34:43 +0400

    initialize

---
+++ db/seeds.rb
@@ -0,0 +1,21 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+user = User.create(login: 'test_user_1', first_name: 'first_name_1',last_name: 'last_name_1')
+
+user.posts.create(title:'first_user_post_1',body: 'first_user_post_1_body')
+
+user.posts.create(title:'first_user_post_2',body: 'first_user_post_2_body')
+
+user.posts.create(title:'first_user_post_3',body: 'first_user_post_3_body')
+
+
+user.posts.each  do |post| 
+	5.times do |i|
+		post.comments.create(title: "Title #{i}",message: 			"Message #{(i+1)*(i+1)}") 
+	end
+end
